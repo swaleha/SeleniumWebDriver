57fbd9ac567d1daed9a5ab28384c1af244cc23a9
ssp <swalehapeerzade@gmail.com>
2022-07-13 03:28:28 -0400

    Update test_search.py

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,30 +1,31 @@
 """
 These tests cover DuckDuckGo search
 """
-
+import pytest
 from pages.search import DuckDuckGoSearchPage 
 from pages.result import DuckDuckGoResultPage
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase',['panda', 'python','polar bear'])
+def test_basic_duckduckgo_search(browser, phrase):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
-    PHRASE = "panda"
     
     # Given the DuckDuckGo search homepage is displayed
     search_page.load()
     
     # When the user searches for "panda"
-    search_page.search(PHRASE)
+    search_page.search(phrase)
     
-    # Then the search result title contains "panda"
-    assert PHRASE in result_page.title()
-    
-    # And the search result query is "panda"
-    assert PHRASE == result_page.search_input_value()
+    # Then the search result query is "panda"
+    assert phrase == result_page.search_input_value()
     
     # And the search result links pertain to "panda"
     titles = result_page.result_link_titles()
-    matches = [title for title in titles if PHRASE.lower() in title.lower()]
+    matches = [title for title in titles if phrase.lower() in title.lower()]
     assert len(matches) > 0
     
-    raise Exception("Incomplete Test")
+    # And the search result title contains "panda"
+    # Placing this assertion in the last guarantees that the page title will be ready
+    assert phrase in result_page.title()
+    
+    # raise Exception("Incomplete Test")
